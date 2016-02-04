eb7b17850c70b6da5a02ea7efebe6240db0e4d18
Tudor-Dan Ravoiu <tudor.ravoiu@plentymarkets.com>
2016-02-04 17:10:34 +0100

    ADD billiger generator

--- src/ElasticExportServiceProvider.hh
+++ src/ElasticExportServiceProvider.hh
@@ -9,18 +9,119 @@ class ElasticExportServiceProvider extends DataExchangeServiceProvider
 {
 	public function register():void
 	{
+		
 	}
 
 	public function exports(ExportPresetContainer $container):void
 	{
+
 		$container->add(
+            'MnoFormat',
+            [
+                'itemBase'=> [
+                    'id',
+                    'producer',
+                ],
+
+				'itemDescription' => [
+					'params' => [
+						'language' => 'de', // TODO change
+					],
+					'fields' => [
+						'name1',
+						'name2',
+						'name3',
+						'description',
+						'shortDescription',
+                    	'technicalData',
+					],
+
+				],
+
+				'variationImageList' => array(
+	                'params' => array(
+	                        'type' => 'all', // Imageurl1  to Imageurl5
+							// 'referenceMarketplace' => get reference,
+	                ),
+	                'fields' => array(
+	                    'imageId',
+	                    'type',
+	                    'fileType',
+	                    'path',
+	                    'position',
+	                    'lastUpdateTimestamp',
+	                    'createTimestamp',
+	                    'cleanImageName',
+	                )
+	            ),
+
+				'variationRetailPrice' => [
+						'price'
+				],
+
+				'variationBarcode' => [
+					'params' => [
+						'EAN' => [
+							'barcodeType' => 'EANGeneral',
+						],
+					],
+					'fields' => [
+						'code',
+						'createdTimestamp',
+						'barcodeId',
+					]
+				],
+            ],
+            BilligerGenerator::class,
+            []
+        );
+
+		$container->add(
+
+        /*
+        $this->formatExportFields = array(
+			'aid',
+			'name',
+			'price',
+			'link',
+			'brand',
+			'ean',
+			'desc',
+			'shop_cat',
+			'image',
+			'dlv_time',
+			'dlv_cost',
+			'ppu',
+			'mpnr',
+		);
+        */
+
 			'BilligerFormat',
 			[
-					'itemBase'=>['id'],
-					'itemDescription'=>['name1'],
-					'variationRetailPrice' => [
-							'price'
+				'itemBase'=> [
+					'id',
+					'producer',
+
+				],
+				'itemDescription' => [
+					'name1'
+				],
+				'variationRetailPrice' => [
+						'price'
+				],
+				'variationBarcode' => [
+					'params' => [
+						'EAN' => [
+							'barcodeType' => 'EANGeneral',
+						],
+					],
+					'fields' => [
+						'code',
+						'createdTimestamp',
+						'barcodeId',
 					]
+				],
+
 			],
 			BilligerGenerator::class,
 			[]
--- src/Generators/BilligerGenerator.hh
+++ src/Generators/BilligerGenerator.hh
@@ -13,11 +13,109 @@ class BilligerGenerator extends CSVGenerator
 	 */
 	protected function generateContent(mixed $resultData, array<FormatSetting> $formatSettings = []):void
 	{
+
 		if($resultData instanceof RecordList)
 		{
+			$settings = array();
 
-			$this->addContent(' #### test billiger ');
+			foreach($formatSettings as $formatSetting)
+			{
+				$settings[$formatSetting->key] = $formatSetting->value;
+			}
+			$this->setDelimiter(";");
 
+			$this->addCSVContent([
+					'aid',
+					'name',
+					'price',
+					//'link',
+					'brand',
+					//'ean',
+					'desc',
+					/*'shop_cat',
+					'image',
+					'dlv_time',
+					'dlv_cost',
+					'ppu',
+					'mpnr',*/
+			]);
+
+			foreach($resultData as $item)
+			{
+				$data = array();
+
+				$data['aid'] = $item->itemBase->id;
+				$data['name'] = $this->getName($item, $settings);
+				$data['price'] = number_format($item->variationRetailPrice->price, 2, '.', '');
+				// link
+				$data['brand'] = $item->itemBase->producer;
+				// ean
+				$data['desc'] = $this->getDescription($item, $settings);
+
+				$this->addCSVContent(array_values($data));
+			}
 		}
+	}
+
+	/**
+	 * Get name.
+	 *
+	 * @param  Record $item
+	 * @param  array  $settings
+	 * @return string
+	 */
+	private function getName(Record $item, array<string, string>$settings):string
+	{
+		if(array_key_exists('nameId', $settings))
+		{
+			switch($settings['nameId'])
+			{
+				case 3:
+					return $item->itemDescription->name3;
+
+				case 2:
+					return $item->itemDescription->name2;
+
+				case 1:
+				default:
+					return $item->itemDescription->name1;
+			}
+		}
+
+		return $item->itemDescription->name1;
+	}
+
+	/**
+	 * Get description.
+	 *
+	 * @param  Record        $item
+	 * @param  array<string, string>$settings
+	 * @return string
+	 */
+	private function getDescription(Record $item, array<string, string>$settings):string
+	{
+		$descriptionLength = 265;
+		if(array_key_exists('descriptionLength', $settings))
+		{
+			$descriptionLength = $settings['descriptionLength'];
+		}
+
+		if(array_key_exists('descriptionType', $settings))
+		{
+				switch($settings['descriptionType'])
+				{
+					case 'shortDescription':
+						return substr(strip_tags($item->itemDescription->shortDescription), 0, $descriptionLength);
+
+					case 'technicalData':
+						return substr(strip_tags($item->itemDescription->technicalData), 0, $descriptionLength);
+
+					case 'description':
+					default:
+						return substr(strip_tags($item->itemDescription->description), 0, $descriptionLength);
+				}
+		}
+
+		return substr(strip_tags($item->itemDescription->description), 0, $descriptionLength);
 	}
 }
