8854108209d857398e5516d2a16d7715de3e3d64
Calin Ciubotariu <calin.ciubotariu@plentymarkets.com>
2016-02-23 15:55:56 +0100

    add Kupona Format

---
+++ src/Generators/KuponaDE.hh
@@ -0,0 +1,102 @@
+<?hh // strict
+namespace ElasticExport\Generators;
+
+use Plenty\Modules\DataExchange\Contracts\CSVGenerator;
+use Plenty\Modules\Helper\Services\ArrayHelper;
+use Plenty\Modules\Item\DataLayer\Models\Record;
+use Plenty\Modules\Item\DataLayer\Models\RecordList;
+use Plenty\Modules\DataExchange\Models\FormatSetting;
+use ElasticExport\Helper\ElasticExportHelper;
+
+
+class KuponaDE extends CSVGenerator
+{
+    /*
+     * @var ElasticExportHelper
+     */
+private ElasticExportHelper $elasticExportHelper;
+
+    /*
+     * @var ArrayHelper
+     */
+private ArrayHelper $arrayHelper;
+
+    /**
+     * KuponaDE constructor.
+     * @param ElasticExportHelper $elasticExportHelper
+     * @param ArrayHelper $arrayHelper
+     */
+    public function __construct(ElasticExportHelper $elasticExportHelper, ArrayHelper $arrayHelper)
+    {
+        $this->elasticExportHelper = $elasticExportHelper;
+        $this->arrayHelper = $arrayHelper;
+    }
+
+    /**
+     * @param mixed $resultData
+     */
+    protected function generateContent(mixed $resultData, array<FormatSetting> $formatSettings = []):void
+	{
+		if($resultData instanceof RecordList)
+		{
+			$settings = $this->arrayHelper->buildMapFromObjectList($formatSettings, 'key', 'value');
+
+			$this->setDelimiter(";");
+
+			$this->addCSVContent([
+					'prod_number',
+                    'prod_name',
+                    'prod_price',
+                    'prod_price_old',
+                    'currency_symbol',
+                    'prod_url',
+                    'category',
+                    'category_url',
+                    'valid_from_date',
+                    'valid_to_date',
+                    'prod_description',
+                    'prod_description_long',
+                    'img_small',
+                    'img_medium',
+                    'img_large',
+                    'ean_code',
+                    'versandkosten',
+                    'lieferzeit',
+                    'platform',
+                    'grundpreis',
+
+                    ]);
+
+			foreach($resultData as $item)
+			{
+				$data = [
+                    'prod_number'           => $item->variationBase->id,
+                    'prod_name'             => $this->elasticExportHelper->getName($item, $settings),
+                    'prod_price'            => number_format($this->elasticExportHelper->getPrice($item, $settings), 2, '.', ''),
+                    'prod_price_old'        => '',
+                    'currency_symbol'       => $item->variationRetailPrice->currency,
+                    'prod_url'              => $this->elasticExportHelper->getUrl($item, $settings, true, false),
+                    'category'              => $this->elasticExportHelper->getCategory($item, $settings),
+                    'category_url'          => '',
+                    'valid_from_date'       => '',
+                    'valid_to_date'         => '',
+                    'prod_description'      => $this->elasticExportHelper->getPreviewText($item, $settings, 256),
+                    'prod_description_long' => $this->elasticExportHelper->getDescription($item, $settings, 256),
+                    'img_small'             => $this->elasticExportHelper->getImageList($item, $settings, ';', 'preview'),
+                    'img_medium'            => $this->elasticExportHelper->getImageList($item, $settings, ';', 'middle'),
+                    'img_large'             => $this->elasticExportHelper->getImageList($item, $settings, ';', 'normal'),
+                    'ean_code'              => $this->elasticExportHelper->getBarcodeByType($item, $settings, ElasticExportHelper::BARCODE_EAN),
+                    'versandkosten'         => number_format($this->elasticExportHelper->getShippingCost($item, $settings), 2, ',', ''),
+                    'lieferzeit'            => $this->elasticExportHelper->getAvailability($item, $settings),
+                    // Additional field that will be filled only for the PLENTY_ID 5208 == gamesload
+                    'platform'              => $settings->get('plentyId') == 5208 ? $item->itemBase->free1 : '',
+
+                    'grundpreis'            => $this->elasticExportHelper->getBasePrice($item, $settings),
+
+				];
+
+				$this->addCSVContent(array_values($data));
+			}
+        }
+    }
+}
---
+++ src/ResultFields/KuponaDE.hh
@@ -0,0 +1,106 @@
+<?hh //strict
+namespace ElasticExport\ResultFields;
+
+use Plenty\Modules\DataExchange\Contracts\ResultFields;
+use Plenty\Modules\DataExchange\Models\FormatSetting;
+use Plenty\Modules\Helper\Services\ArrayHelper;
+
+/**
+ * Class KuponaDE
+ * @package ElasticExport\ResultFields
+ */
+class KuponaDE extends ResultFields
+{
+    /*
+	 * @var ArrayHelper
+	 */
+private ArrayHelper $arrayHelper;
+
+    /**
+     * KuponaDE constructor.
+     * @param ArrayHelper $arrayHelper
+     */
+    public function __construct(ArrayHelper $arrayHelper)
+    {
+        $this->arrayHelper = $arrayHelper;
+    }
+
+    /**
+     * Generate result fields.
+     * @param  array<FormatSetting> $formatSettings = []
+     * @return array
+     */
+    public function generateResultFields(array<FormatSetting> $formatSettings = []):array<string, mixed>
+    {
+        $settings = $this->arrayHelper->buildMapFromObjectList($formatSettings, 'key', 'value');
+
+
+        return [
+            'itemBase'=> [
+                'id',
+                'producer',
+            ],
+
+            'itemDescription' => [
+                'params' => [
+                    'language' => $settings->get('lang') ? $settings->get('lang') : 'de',
+                ],
+                'fields' => [
+                    'name1',
+                    'name2',
+                    'name3',
+                    'description',
+                    'shortDescription',
+                    'technicalData',
+                    'urlContent'
+                ],
+            ],
+
+            'variationImageList' => [
+                'params' => [
+                    'type' => 'variation',
+                ],
+                'fields' => [
+                    'type',
+                    'path',
+                    'position',
+                ]
+
+            ],
+
+            'variationBase' => [
+                'availability',
+                'attributeValueSetId',
+                'model',
+                'limitOrderByStockSelect',
+                'unitId',
+                'content',
+            ],
+
+            'variationRetailPrice' => [
+                'price',
+            ],
+
+            'variationStandardCategory' => [
+                'params' => [
+                    'plentyId' => $settings->get('plentyId'),
+                ],
+                'fields' => [
+                    'categoryId',
+                    'plentyId',
+                    'manually',
+                ],
+            ],
+
+            'variationBarcode' => [
+                'params' => [
+                    'barcodeType' => $settings->get('barcode') ? $settings->get('barcode') : 'EAN',
+                ],
+                'fields' => [
+                    'code',
+                    'barcodeId',
+                ]
+            ],
+        ];
+    }
+}
