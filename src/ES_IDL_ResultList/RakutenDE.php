465bc44c529f2be7aa30fa9a7aadf7f0d6c9849a
Max-Nils Brüschke <max.brueschke@plentymarkets.com>
2017-01-20 13:45:55 +0100

    rakutenDE es freeFields

--- src/ES_IDL_ResultList/RakutenDE.php
+++ src/ES_IDL_ResultList/RakutenDE.php
@@ -26,26 +26,6 @@ class RakutenDE
             $resultFields = array(
                 'itemBase' => array(
                     'id',
-                    'free1',
-                    'free2',
-                    'free3',
-                    'free4',
-                    'free5',
-                    'free6',
-                    'free7',
-                    'free8',
-                    'free9',
-                    'free10',
-                    'free11',
-                    'free12',
-                    'free13',
-                    'free14',
-                    'free15',
-                    'free16',
-                    'free17',
-                    'free18',
-                    'free19',
-                    'free20',
                 ),
 
                 'variationBase' => array(
